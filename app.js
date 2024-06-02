72dffdc831af2d35ea8662c0f7e90667b1ace1e5
Lexi Cheng <girlmeetsworld.cys@gmail.com>
2024-06-02 18:54:41 +0800

    Add new page to put my presentation at school and update style

--- app.js
+++ app.js
@@ -7,3 +7,19 @@ window.addEventListener("scroll", () => {
     nav.style.boxShadow = "0 10px 6px -6px #777";
   }
 });
+
+let buttons = document.querySelector(".presentation-buttons");
+let embedDiv = document.querySelector(".embed-div");
+buttons.addEventListener("click", (e) => {
+  console.log(e.target.id);
+  if (e.target.id == "presentation1") {
+    embedDiv.innerHTML =
+      '<embed src="./presentation/資料庫管理期末書面報告_主題為台灣全國疫苗施打紀錄.pdf" type="">';
+  } else if (e.target.id == "presentation2") {
+    embedDiv.innerHTML =
+      '<embed src="./presentation/醫觸即發_專題展覽報告.pdf" type="">';
+  } else if (e.target.id == "presentation3") {
+    embedDiv.innerHTML =
+      '<embed src="./presentation/物聯網第21組小專.pdf" type="">';
+  }
+});
